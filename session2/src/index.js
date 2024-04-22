e2b14cf6da4d7460ddc254fbf34ca8d6351d6cdb
aditya <ammanabrolu.aditya12@gmail.com>
2024-04-22 10:41:10 +0530

    Update index.js

--- session2/src/index.js
+++ session2/src/index.js
@@ -1,24 +1,24 @@
-// let i = 0;
-// const interval = setInterval(() => {if(i == 3) clearInterval(interval);console.log(i);i++;}, 1500);
+let i = 0;
+const interval = setInterval(() => {if(i == 3) clearInterval(interval);console.log(i);i++;}, 1500);
 
 
-// let promise = new Promise((resolve, reject) => {
-//     reject(new Error('Promise rejected'))
-// })
+let promise = new Promise((resolve, reject) => {
+    reject(new Error('Promise rejected'))
+})
 
-// promise.then(c => console.log(c)).catch(err => console.error(err))
+promise.then(c => console.log(c)).catch(err => console.error(err))
 
-// const generatorNext = generatorFunc();
-
-
-// for(let j=0;j<20;j++)
-// console.log(generatorNext.next());
+const generatorNext = generatorFunc();
 
 
-// function* generatorFunc() {
-//     yield 1;
-//     yield 2;
-//     yield 3;
-// }
+for(let j=0;j<20;j++)
+console.log(generatorNext.next());
+
+
+function* generatorFunc() {
+    yield 1;
+    yield 2;
+    yield 3;
+}
 
 
